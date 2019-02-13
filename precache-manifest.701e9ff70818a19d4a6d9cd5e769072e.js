cf1985a6537f0544a67dff3e5918c3bfc0799142
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-02-13 09:00:06 +0000

    Deploy KhromovE/vue-snake-game to github.com/KhromovE/vue-snake-game.git:gh-pages

---
+++ precache-manifest.701e9ff70818a19d4a6d9cd5e769072e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f6664ee78e7c4282f90f",
+    "url": "/vue-snake-game/js/app.09e25f39.js"
+  },
+  {
+    "revision": "7dad3511253101dd5803",
+    "url": "/vue-snake-game/js/chunk-27774ac0.4fdbd900.js"
+  },
+  {
+    "revision": "104121a013e46efbedc9",
+    "url": "/vue-snake-game/js/chunk-vendors.9b8de092.js"
+  },
+  {
+    "revision": "10884e1225b8eb470a720b4c18c166b9",
+    "url": "/vue-snake-game/index.html"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/vue-snake-game/robots.txt"
+  }
+];
